52092b8db24802f61d4af93199e09aa17b070a85
张瑞宇 <15201231801@163.com>
2021-11-03 14:55:59 +0800

    升级一下

--- lib/multi_file_picker.dart
+++ lib/multi_file_picker.dart
@@ -4,15 +4,15 @@ import 'dart:io';
 import 'package:flutter/services.dart';
 
 class MultiFilePicker {
-  static const MethodChannel _channel =
-      const MethodChannel('multi_file_picker');
+  static const MethodChannel _channel = MethodChannel('multi_file_picker');
 
-  static Future<List<String>?> select({List<String>? type}) async {
-    if (Platform.isAndroid)
-      return _channel.invokeListMethod<String>('select', {
+  static Future<List<String>> select({List<String>? type}) async {
+    if (Platform.isAndroid) {
+      return (await _channel.invokeListMethod<String>('select', {
         'type': type ?? ["aac", "mp3", "wav", "m4a", "flac"]
-      });
-    else
+      }))!;
+    } else {
       return [];
+    }
   }
 }
